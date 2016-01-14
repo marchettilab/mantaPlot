7d0fa83a63e99ef637534de8e2d76e47f0d4c19a
Rob Lampe <robhlampe@gmail.com>
2016-01-14 11:27:35 -0500

    Bugfix: Issue with colors

--- mantaPlot.R
+++ mantaPlot.R
@@ -84,19 +84,19 @@ mantaPlot <- function(counts, exactTest, group1, group2, taxa = 4,
   abline(h = 0)
   legend_colors <- c("#1F77B4", "#D62728", "#2CA02C", "#FF7F0E")
   legend_colors[taxa+1] <- "#7F7F7F"
-  legend('topright', inset = 0.02, names(df), fill = colors, cex = 0.75)
+  legend('topright', inset = 0.02, names(df), fill = legend_colors, cex = 0.75)
   
   for (ko in kos) {
     x = exactTest[exactTest$KO == ko,]$logCPM
     y = exactTest[exactTest$KO == ko,]$logFC
     pie_percents <- as.numeric(df[ko,])
     radius <- (x + abs(y))/ 100
-    colors <- legend_colors
+    pie_colors <- legend_colors
     border = "black"
     if (exactTest[exactTest$KO == ko,]$PValue > 0.05) {
-      colors <- paste(colors, "33", sep='')
+      pie_colors <- paste(pie_colors, "33", sep='')
       border = "#4D4D4D"
     }
-    floating.pie(x, y, pie_percents, radius = radius, col = colors, border = border)
+    floating.pie(x, y, pie_percents, radius = radius, col = pie_colors, border = border)
   }
 }
